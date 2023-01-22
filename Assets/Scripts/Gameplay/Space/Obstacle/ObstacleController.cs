873bf5d3cef088d83cb0589345bc1d44c51a85b4
NewdayTurnaway <moba545@gmail.com>
2023-01-22 23:51:13 +0300

    Fix Obstacles

--- Assets/Scripts/Gameplay/Space/Obstacle/ObstacleController.cs
+++ Assets/Scripts/Gameplay/Space/Obstacle/ObstacleController.cs
@@ -1,33 +1,75 @@
 ﻿using Abstracts;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Gameplay.Space.Obstacle
 {
     public sealed class ObstacleController : BaseController
     {
+        private const int SearchDistance = 10000;
+
         private readonly ObstacleView _obstacleView;
+        private readonly Collider2D _obstacleCollider;
         private readonly float _obstacleForce;
 
-        private readonly Dictionary<UnitView, ObstacleContactInfo> _unitCollection = new();
+        private readonly Dictionary<UnitView, Vector3> _unitCollection = new();
 
         public ObstacleController(ObstacleView obstacleView, float obstacleForce)
         {
             _obstacleView = obstacleView;
+            
+            if (obstacleView.TryGetComponent<CompositeCollider2D>(out var compositeCollider2D))
+            {
+                _obstacleCollider = compositeCollider2D;
+            }
+            else
+            {
+                _obstacleCollider = obstacleView.GetComponent<Collider2D>();
+            }
+
             _obstacleForce = obstacleForce;
 
             _obstacleView.OnTriggerEnter += OnObstacleEnter;
-            _obstacleView.OnTriggerStay += OnObstacleStay;
             _obstacleView.OnTriggerExit += OnObstacleExit;
 
             AddGameObject(obstacleView.gameObject);
+
+            EntryPoint.SubscribeToFixedUpdate(Repulsion);
         }
 
         protected override void OnDispose()
         {
             _obstacleView.OnTriggerEnter -= OnObstacleEnter;
-            _obstacleView.OnTriggerStay -= OnObstacleStay;
             _obstacleView.OnTriggerExit -= OnObstacleExit;
+
+            _unitCollection.Clear();
+
+            EntryPoint.UnsubscribeFromFixedUpdate(Repulsion);
+        }
+
+        private void Repulsion()
+        {
+            if (!_unitCollection.Any())
+            {
+                return;
+            }
+
+            foreach (var item in _unitCollection)
+            {
+                var rigidbody = item.Key.GetComponent<Rigidbody2D>();
+                var anchorPoint = (Vector3)_obstacleCollider.ClosestPoint(rigidbody.transform.position);
+                var vectorDirection = anchorPoint - rigidbody.transform.position;
+
+                if (anchorPoint == rigidbody.transform.position)
+                {
+                    vectorDirection = rigidbody.transform.position - item.Value;
+                }
+
+                anchorPoint += vectorDirection.normalized;
+                var forceDirection = (rigidbody.transform.position - anchorPoint).normalized;
+                rigidbody.AddForce(forceDirection * _obstacleForce, ForceMode2D.Impulse);
+            }
         }
 
         private void OnObstacleEnter(UnitView unitView)
@@ -37,31 +79,24 @@ namespace Gameplay.Space.Obstacle
                 return;
             }
 
-            var rigidbody = unitView.GetComponent<Rigidbody2D>();
-            var currentDirection = unitView.transform.TransformDirection(Vector3.up).normalized;
-            var dot = Vector2.Dot(currentDirection, rigidbody.velocity.normalized);
+            var closestPoint = _obstacleCollider.ClosestPoint(unitView.transform.position);
             
-            var info = new ObstacleContactInfo() { StartDirection = currentDirection, IsVelocitySameDirection = dot >= 0 };
-            
-            _unitCollection.Add(unitView, info);
-        }
-        
-        private void OnObstacleStay(UnitView unitView)
-        {
-            if (_unitCollection.TryGetValue(unitView, out var obstacleContactInfo))
+            if (closestPoint == (Vector2)unitView.transform.position)
             {
-                var rigidbody = unitView.GetComponent<Rigidbody2D>();
-                var direction = obstacleContactInfo.IsVelocitySameDirection ? -obstacleContactInfo.StartDirection : obstacleContactInfo.StartDirection;
-                rigidbody.AddForce(direction * _obstacleForce, ForceMode2D.Impulse);
+                var searchPoint = unitView.transform.TransformPoint(Vector3.down * SearchDistance);
+                closestPoint = _obstacleCollider.ClosestPoint(searchPoint);
             }
+
+            _unitCollection.Add(unitView, closestPoint);
         }
-        
+
         private void OnObstacleExit(UnitView unitView)
         {
-            if (_unitCollection.ContainsKey(unitView))
+            if (!_unitCollection.ContainsKey(unitView))
             {
-                _unitCollection.Remove(unitView);
+                return;
             }
+            _unitCollection.Remove(unitView);
         }
     }
 }
