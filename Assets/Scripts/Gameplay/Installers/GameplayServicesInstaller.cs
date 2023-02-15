307a9738616b50b34f048d629a49522c26c79c8d
Zadir7 <Zadir7@mail.ru>
2023-02-15 23:46:34 +0700

    Player input system

--- Assets/Scripts/Gameplay/Installers/GameplayServicesInstaller.cs
+++ Assets/Scripts/Gameplay/Installers/GameplayServicesInstaller.cs
@@ -1,19 +1,36 @@
 using Gameplay.Services;
+using UnityEngine;
 using Zenject;
 
 namespace Gameplay.Installers
 {
     public class GameplayServicesInstaller : MonoInstaller
     {
+        [field: SerializeField] public float VerticalAxisInputMultiplier { get; set; }
         public override void InstallBindings()
         {
             InstallLevelProgressService();
+            InstallPlayerInput();
         }
-        
+
         private void InstallLevelProgressService()
         {
             Container
                 .Bind<CurrentLevelProgress>()
+                .AsSingle()
+                .NonLazy();
+        }
+
+        private void InstallPlayerInput()
+        {
+            Container
+                .Bind<float>()
+                .FromInstance(VerticalAxisInputMultiplier)
+                .AsSingle()
+                .NonLazy();
+            
+            Container
+                .BindInterfacesAndSelfTo<PlayerInput>()
                 .AsSingle()
                 .NonLazy();
         }
---
+++ Assets/Scripts/Gameplay/Services/PlayerInput.cs
@@ -0,0 +1,92 @@
+using System;
+using Services;
+using UnityEngine;
+
+namespace Gameplay.Services
+{
+    public class PlayerInput : IDisposable
+    {
+        private readonly Updater _updater;
+        private readonly float _verticalAxisInputMultiplier;
+
+        public event Action<Vector3> MousePositionInput = _ => { };
+        
+        public event Action<float> VerticalAxisInput = _ => { };
+
+        public event Action<bool> PrimaryFireInput = _ => { };
+        public event Action<bool> ChangeWeaponInput = _ => { };
+        public event Action<bool> NextLevelInput = _ => { };
+        public event Action<bool> MapInput = _ => { };
+        
+        private const string Vertical = "Vertical";
+        private const KeyCode PrimaryFire = KeyCode.Mouse0;
+        private const KeyCode ChangeWeapon = KeyCode.Q;
+        private const KeyCode NextLevel = KeyCode.Return;
+        private const KeyCode Map = KeyCode.Tab;
+        
+        
+
+        public PlayerInput(Updater updater, float verticalAxisInputMultiplier)
+        {
+            _updater = updater;
+            _verticalAxisInputMultiplier = verticalAxisInputMultiplier;
+            
+            _updater.SubscribeToUpdate(CheckVerticalInput);
+            _updater.SubscribeToUpdate(CheckFiringInput);
+            _updater.SubscribeToUpdate(CheckMousePositionInput);
+            _updater.SubscribeToUpdate(CheckChangeWeaponInput);
+            _updater.SubscribeToUpdate(CheckNextLevelInput);
+            _updater.SubscribeToUpdate(CheckMapInput);
+        }
+
+        public void Dispose()
+        {
+            _updater.UnsubscribeFromUpdate(CheckVerticalInput);
+            _updater.UnsubscribeFromUpdate(CheckFiringInput);
+            _updater.UnsubscribeFromUpdate(CheckMousePositionInput);
+            _updater.UnsubscribeFromUpdate(CheckChangeWeaponInput);
+            _updater.UnsubscribeFromUpdate(CheckNextLevelInput);
+            _updater.UnsubscribeFromUpdate(CheckMapInput);
+        }
+        
+        private void CheckVerticalInput()
+        {
+            float verticalOffset = UnityEngine.Input.GetAxis(Vertical);
+            float inputValue = CalculateInputValue(verticalOffset, _verticalAxisInputMultiplier);
+            VerticalAxisInput(inputValue);
+        }
+
+        private void CheckFiringInput()
+        {
+            bool value = UnityEngine.Input.GetKey(PrimaryFire);
+            PrimaryFireInput(value);
+        }
+
+        private void CheckMousePositionInput()
+        {
+            Vector3 value = UnityEngine.Input.mousePosition;
+            MousePositionInput(value);
+        }
+
+        private void CheckChangeWeaponInput()
+        {
+            bool value = UnityEngine.Input.GetKeyDown(ChangeWeapon);
+            ChangeWeaponInput(value);
+        }
+
+        private void CheckNextLevelInput()
+        {
+            bool value = UnityEngine.Input.GetKeyDown(NextLevel);
+            NextLevelInput(value);
+        }
+        
+        private void CheckMapInput()
+        {
+            bool value = UnityEngine.Input.GetKey(Map);
+            MapInput(value);
+        }
+
+        private static float CalculateInputValue(float axisOffset, float inputMultiplier)
+            => axisOffset * inputMultiplier;
+    }
+}
