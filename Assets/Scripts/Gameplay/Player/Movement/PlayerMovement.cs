2b02746801a64a1a46e62fff9bcd927d268375be
NewdayTurnaway <moba545@gmail.com>
2023-02-22 17:42:58 +0300

    Update PlayerMovement.cs

--- Assets/Scripts/Gameplay/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Gameplay/Player/Movement/PlayerMovement.cs
@@ -1,6 +1,8 @@
 using System;
 using Gameplay.Input;
 using Gameplay.Movement;
+using UI.Game;
+using UI.Services;
 using UnityEngine;
 
 namespace Gameplay.Player.Movement
@@ -12,15 +14,20 @@ namespace Gameplay.Player.Movement
         private readonly Rigidbody2D _rigidbody;
         private readonly Transform _transform;
 
+        private readonly PlayerSpeedometerView _speedometerView;
+
         public float CurrentSpeed => _model.CurrentSpeed;
         public float MaxSpeed => _model.MaxSpeed;
 
-        public PlayerMovement(PlayerView playerView, PlayerInput playerInput, UnitMovementModelFactory movementModelFactory, UnitMovementConfig movementConfig)
+        public PlayerMovement(PlayerView playerView, PlayerInput playerInput, PlayerInfoService playerInfoService, UnitMovementModelFactory movementModelFactory, UnitMovementConfig movementConfig)
         {
             _playerInput = playerInput;
             _model = movementModelFactory.Create(movementConfig);
             _rigidbody = playerView.GetComponent<Rigidbody2D>();
             _transform = playerView.transform;
+
+            _speedometerView = playerInfoService.PlayerSpeedometerView;
+            _speedometerView.Init(GetSpeedometerTextValue(_model.CurrentSpeed, _model.MaxSpeed));
 
             _playerInput.VerticalAxisInput += HandleVerticalInput;
         }
@@ -36,6 +43,8 @@ namespace Gameplay.Player.Movement
             {
                 _model.Accelerate(newInputValue > 0);
             }
+
+            _speedometerView.UpdateText(GetSpeedometerTextValue(_model.CurrentSpeed, _model.MaxSpeed));
 
             if (CurrentSpeed != 0)
             {
@@ -54,6 +63,15 @@ namespace Gameplay.Player.Movement
             {
                 _model.StopMoving();
             }
+        }
+
+        private string GetSpeedometerTextValue(float currentSpeed, float maximumSpeed)
+        {
+            return currentSpeed switch
+            {
+                < 0 => "R",
+                _ => $"SPD: {Mathf.RoundToInt(currentSpeed / maximumSpeed * 100)}"
+            };
         }
     }
 }
