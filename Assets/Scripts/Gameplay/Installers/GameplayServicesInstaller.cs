672e2c63387a52166f70190cacde02437fa3d5b7
NewdayTurnaway <moba545@gmail.com>
2023-02-22 18:12:49 +0300

    Add game background

--- Assets/Scripts/Gameplay/Installers/GameplayServicesInstaller.cs
+++ Assets/Scripts/Gameplay/Installers/GameplayServicesInstaller.cs
@@ -1,6 +1,8 @@
+using Gameplay.Background;
 using Gameplay.Input;
 using Gameplay.Movement;
 using Gameplay.Services;
+using Scriptables;
 using UnityEngine;
 using Zenject;
 
@@ -8,11 +10,15 @@ namespace Gameplay.Installers
 {
     public sealed class GameplayServicesInstaller : MonoInstaller
     {
+        [field: SerializeField] public BackgroundConfig BackgroundConfig { get; private set; }
+        [field: SerializeField] public BackgroundView BackgroundView { get; private set; }
+
         [field: SerializeField] public PlayerInputConfig PlayerInputConfig { get; private set; }
 
         public override void InstallBindings()
         {
             InstallCurrentGameState();
+            InstallBackground();
             InstallPlayerInput();
             InstallUnitMovement();
         }
@@ -21,6 +27,26 @@ namespace Gameplay.Installers
         {
             Container
                 .Bind<CurrentGameState>()
+                .AsSingle()
+                .NonLazy();
+        }
+
+        private void InstallBackground()
+        {
+            Container
+                .Bind<BackgroundConfig>()
+                .FromInstance(BackgroundConfig)
+                .AsSingle()
+                .NonLazy();
+
+            Container
+                .Bind<BackgroundView>()
+                .FromInstance(BackgroundView)
+                .AsSingle()
+                .NonLazy();
+
+            Container
+                .BindInterfacesAndSelfTo<Background.Background>()
                 .AsSingle()
                 .NonLazy();
         }
