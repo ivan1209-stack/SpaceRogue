b1703579010340c9cc735426e96a77d741bb38ea
NewdayTurnaway <moba545@gmail.com>
2023-02-23 16:04:19 +0300

    Add PlayerSpeedometerService & Refactoring

--- Assets/Scripts/Gameplay/Player/Movement/PlayerMovementFactory.cs
+++ Assets/Scripts/Gameplay/Player/Movement/PlayerMovementFactory.cs
@@ -1,8 +1,17 @@
+using System;
 using Zenject;
 
 namespace Gameplay.Player.Movement
 {
     public class PlayerMovementFactory : PlaceholderFactory<PlayerView, PlayerMovement>
     {
+        public event Action<PlayerMovement> PlayerMovementCreated = _ => { };
+
+        public override PlayerMovement Create(PlayerView param)
+        {
+            var playerMovement = base.Create(param);
+            PlayerMovementCreated?.Invoke(playerMovement);
+            return playerMovement;
+        }
     }
 }
---
+++ Assets/Scripts/UI/Services/PlayerSpeedometerService.cs
@@ -0,0 +1,58 @@
+using Gameplay.Input;
+using Gameplay.Player.Movement;
+using System;
+using UI.Game;
+using UnityEngine;
+
+namespace UI.Services
+{
+    public sealed class PlayerSpeedometerService : IDisposable
+    {
+        private readonly PlayerSpeedometerView _playerSpeedometerView;
+        private readonly PlayerInput _playerInput;
+        private readonly PlayerMovementFactory _movementFactory;
+
+        private PlayerMovement _playerMovement;
+
+        public PlayerSpeedometerService(PlayerInfoView playerInfoView, PlayerInput playerInput, PlayerMovementFactory movementFactory)
+        {
+            _playerSpeedometerView = playerInfoView.PlayerSpeedometerView;
+            _playerInput = playerInput;
+            _movementFactory = movementFactory;
+
+            _playerSpeedometerView.gameObject.SetActive(false);
+
+            _movementFactory.PlayerMovementCreated += OnPlayerMovementCreated;
+        }
+
+        public void Dispose()
+        {
+            _movementFactory.PlayerMovementCreated -= OnPlayerMovementCreated;
+            _playerInput.VerticalAxisInput -= UpdateSpeedometer;
+        }
+
+        private void OnPlayerMovementCreated(PlayerMovement playerMovement)
+        {
+            _playerMovement = playerMovement;
+
+            _playerSpeedometerView.gameObject.SetActive(true);
+            _playerSpeedometerView.Init(GetSpeedometerTextValue(_playerMovement.CurrentSpeed, _playerMovement.MaxSpeed));
+
+            _playerInput.VerticalAxisInput += UpdateSpeedometer;
+        }
+
+        private void UpdateSpeedometer(float obj)
+        {
+            _playerSpeedometerView.UpdateText(GetSpeedometerTextValue(_playerMovement.CurrentSpeed, _playerMovement.MaxSpeed));
+        }
+
+        private string GetSpeedometerTextValue(float currentSpeed, float maximumSpeed)
+        {
+            return currentSpeed switch
+            {
+                < 0 => "R",
+                _ => $"SPD: {Mathf.RoundToInt(currentSpeed / maximumSpeed * 100)}"
+            };
+        }
+    }
+}
