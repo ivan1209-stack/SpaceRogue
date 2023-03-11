0be84b64f4e7e8ebae91939719781dc891c810fc
Bharmaglot <46847706+Bharmaglot@users.noreply.github.com>
2023-03-11 14:07:34 +0300

    part_one_fix_upd

--- Assets/Scripts/Gameplay/Space/SpaceObjects/SpaceObjectsEffects/Scriptables/DamageAuraConfig.cs
+++ Assets/Scripts/Gameplay/Space/SpaceObjects/SpaceObjectsEffects/Scriptables/DamageAuraConfig.cs
@@ -6,7 +6,7 @@ namespace SpaceObjects
     public class DamageAuraConfig : SpaceObjectEffectConfig
     {
         [field: SerializeField, Header("AuraBody")] public AreaEffectView Prefab { get; private set; }
-        [field: SerializeField, Range(1f, 5f), Header("Radius")] public float Radius { get; private set; } = 1;
+        [field: SerializeField, Min(1f), Header("Radius")] public float Radius { get; private set; } = 1;
         [field: SerializeField, Min(0), Header("AuraDamage")] public int Damage { get; private set; } = 0;
         [field: SerializeField, Min(0.1f)] public float DamageCooldown { get; private set; } = 0.1f;
     }
