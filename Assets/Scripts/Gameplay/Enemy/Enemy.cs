a015231d77a6599011854d861aec1fdf84af6884
NewdayTurnaway <moba545@gmail.com>
2023-03-16 14:14:32 +0300

    Enemy initialization in factory

--- Assets/Scripts/Gameplay/Enemy/Enemy.cs
+++ Assets/Scripts/Gameplay/Enemy/Enemy.cs
@@ -1,29 +1,21 @@
 using System;
-using Gameplay.Enemy.Scriptables;
 using Gameplay.Survival;
-using UnityEngine;
 
 namespace Gameplay.Enemy
 {
     public sealed class Enemy : IDisposable
     {
-        private readonly EnemyConfig _enemyConfig;
-
         public EnemyView EnemyView { get; }
         public EntitySurvival Survival { get; }
 
-        public Enemy(
-            Vector2 spawnPoint,
-            EnemyConfig enemyConfig,
-            EnemyViewFactory enemyViewFactory)
+        public Enemy(EnemyView enemyView)
         {
-            _enemyConfig = enemyConfig;
-            EnemyView = enemyViewFactory.Create(spawnPoint, enemyConfig);
+            EnemyView = enemyView;
         }
 
         public void Dispose()
         {
-            UnityEngine.Object.Destroy(EnemyView);
+            UnityEngine.Object.Destroy(EnemyView.gameObject);
         }
     }
 }
--- Assets/Scripts/Gameplay/Enemy/EnemyFactory.cs
+++ Assets/Scripts/Gameplay/Enemy/EnemyFactory.cs
@@ -6,5 +6,17 @@ namespace Gameplay.Enemy
 {
     public sealed class EnemyFactory : PlaceholderFactory<Vector2, EnemyConfig, Enemy>
     {
+        private readonly EnemyViewFactory _enemyViewFactory;
+
+        public EnemyFactory(EnemyViewFactory enemyViewFactory)
+        {
+            _enemyViewFactory = enemyViewFactory;
+        }
+
+        public override Enemy Create(Vector2 spawnPoint, EnemyConfig enemyConfig)
+        {
+            var enemyView = _enemyViewFactory.Create(spawnPoint, enemyConfig);
+            return new(enemyView);
+        }
     }
 }
